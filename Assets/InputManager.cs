efbaea9697af9c777d06c706b5bdcbda3b0d5c7b
EliKvar <46699047+EliKvar@users.noreply.github.com>
2019-09-05 11:03:15 -0600

    start
    start

---
+++ Assets/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InputManager : MonoBehaviour
+{
+    public Text outText;
+    public Button enterButton;
+    public InputField textIn;
+
+
+    void Start()
+    {
+        outText.text = "";
+        
+    }
+
+    public void EnterPressed()
+    {
+        outText.text  =Utilities.ProcessText(textIn.text);
+
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Utilities.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities 
+{
+    public static string ProcessText(string input)
+    {
+        //determine if input is num or string, return word if string return number if num.
+
+
+        float number;
+        int counter = 0;
+        int SIZE = input.Length;
+
+        char[] characters = new char[SIZE]; 
+        
+
+       // foreach (char c in input) {
+         //   characters = input[counter];
+            
+            if (float.TryParse(input, out number)) 
+            {
+            // float numbers[] = 
+            //  foreach (float)
+
+            counter++;
+                return ($"{input} is a number");
+            }
+            else
+            {
+
+                return ($"{input} is a string");
+            }
+        //}
+
+
+    } 
+
+   
+}
