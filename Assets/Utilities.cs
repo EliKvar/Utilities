6b8a8d70d2dd9074395c9d8e1d16f30f74a7fd97
EliKvar <46699047+EliKvar@users.noreply.github.com>
2019-09-11 20:54:36 -0600

    added nothing

--- Assets/Utilities.cs
+++ Assets/Utilities.cs
@@ -2,29 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Utilities 
+public static class Utilities
 {
     public static string ProcessText(string input)
     {
         //determine if input is num or string, return word if string return number if num.
 
 
-      //  float number;
-       // int counter = 0;
-       // int SIZE = input.Length;
+        //  float number;
+        // int counter = 0;
+        // int SIZE = input.Length;
 
         //char[] characters = new char[SIZE]; 
-        
 
-        foreach (char c in input) {
-         //   characters = input[counter];
-            
-            if (char.IsLetter(c)) 
+
+        foreach (char c in input)
+        {
+            //   characters = input[counter];
+
+            if (char.IsLetter(c))
             {
-            // float numbers[] = 
-            //  foreach (float)
+                // float numbers[] = 
+                //  foreach (float)
 
-           // counter++;
+                // counter++;
                 return ($"{c} is a string");
             }
             else
@@ -38,6 +39,4 @@ public static class Utilities
 
 
     }
-
-
 }
