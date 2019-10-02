4e88c0975b77658d157fc23d7880166285bd6faf
EliKvar <46699047+EliKvar@users.noreply.github.com>
2019-10-02 14:24:39 -0600

    tower defence game
    Stone tower working. need to add different projectile logic

---
+++ SectionV Tower Defence/TowerDefense Chapter 18 Final/Assets/Scripts/Game/Tower/Weapons/Stone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : FollowProjectile
+{
+    public float damage;
+
+    protected override void OnHitEnemy()
+    {
+        enemyToFollow.TakeDamage(damage);
+        Destroy(gameObject);
+    }
+}
